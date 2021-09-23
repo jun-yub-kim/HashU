d94fea53605a7b917d586b4dd0af66df14ba38aa
yunseonyeong <64634970+yunseonyeong@users.noreply.github.com>
2021-09-24 04:34:20 +0900

    feat : create account app's urls

---
+++ src/HashU/account/urls.py
@@ -0,0 +1,14 @@
+from os import name
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+  path('logout/',  views.logout, name="logout"),
+  path('login/', views.login_view, name="login_view"),
+  path('loginAction/', views.login_action, name="login_action"),
+  path('join/', views.join_view, name="join_view"),
+  path('joinAction/', views.join_action, name="join_action"),
+  path('joinEmailOverap/', views.join_email_overap, name="join_email_overap"),
+  path('is-login/', views.is_login),
+]
